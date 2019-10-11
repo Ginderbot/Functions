795284c4bb46f94b992544492b2dfce2956e4628
Student <Student@304-1>
2019-10-11 21:12:18 +0300

    X0_1

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //#define Factorial
 
 #define POWER
+#define fibonachi_1
 int factorial(int n)
 {
 	if (n == 0)return 1;
@@ -19,14 +20,9 @@ double power(double a, int n)
 	if (n > 0)return a * power(a, n - 1);
 	else return 1 / a*power(a, n + 1);*/
 }
-int fibonacci(int a, int b, int c)
-{
-	a+b=c
-	if ()
-	{
+void fibonacci_1(int n, int a = 0, int b = 1);
+void fibonacci_2(int n,int num=0, int a = 0, int b = 1);
 
-	}
-}
 
 void main()
 {
@@ -36,11 +32,43 @@ void main()
 	cout << "¬ведите число, дл€ вычислени€ факториала: "; cin >> n;
 	cout << n << "!=" << factorial(n) << endl;
 #endif // FACTORIAL
-	int a;
+	/*int a;
 	double n;
 	cout << "¬ведите основание степени"; cin >> a;
 	cout << "¬ведите степень"; cin >> n;
-	cout << a << "^" << n << "=" << power(a, n)<<endl;
-	cout << fibonacci(a);
+	cout << a << "^" << n << "=" << power(a, n)<<endl;*/
+#ifdef fibonachi_1
+	int n;
+	cout << "¬ведите до какого числа отобразить р€д ‘ибоначи: "; cin >> n;
+	fibonacci_1(n);
+
+	cout << "¬ведите количество р€да ‘ибоначи: "; cin >> n;
+	fibonacci_2(n);
+#endif // fibonachi_1
+
 }
+
+
+void fibonacci_1(int n, int a, int b)
+{
+	if (a > n)
+	{
+		cout << endl;
+		return;
+	}
+	cout << a << "\t";
+	fibonacci_1(n, b, a + b);
+}
+void fibonacci_2(int n,int num, int a, int b)
+{
+	if (num >= n)
+	{
+		cout << endl;
+		return;
+	}
+	cout << a << "\t";
+	num++;
+	fibonacci_2(n,num, b, a + b);
+}
+
 
