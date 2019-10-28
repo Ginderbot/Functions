87685b89a0b15473f3298c025d692545f0abe79d
Student <Student@304-1>
2019-10-28 20:34:22 +0200

    +

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -2,18 +2,28 @@
 using namespace std;
 
 void Exchange(int* a, int* b);
+template<typename T>
+void Xor(T*a, T*b);
 
 
 void main()
 {
-	int a = 2, b = 3;
+	int a = 10, b = 6;
 	cout << a << "\t" << b << endl;
-	Exchange(&a, &b);
+	//Exchange(&a, &b);
+	Xor(&a, &b);
 	cout << a << "\t" << b << endl;
 }
 void Exchange(int* a, int* b)
 {
-	*a = *a + *b; 
-	*b = *a - *b; 
-	*a = *a - *b; 
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+template<typename T>
+void Xor(T*a, T*b)
+{
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
 }
