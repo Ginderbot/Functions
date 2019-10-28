bac0da83c68d8013d0cce20c306e4673c1917a81
Student <Student@304-1>
2019-10-28 21:13:06 +0200

    +

---
+++ Project3/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int a = 2;
+	int& ra=a;
+	ra = 666;
+	cout << a << endl;
+	cout << &a << endl;
+	cout << &ra << endl;
+}
--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
+//#define BY_POINTER
+#define BY_REFERENSE
+
+#ifdef BY_POINTER
 void Exchange(int* a, int* b);
 template<typename T>
 void Xor(T*a, T*b);
+#endif // BY_POINTER
+
+#ifdef BY_REFERENSE
+template<typename T>
+void Xor(int &a, int&b);
+#endif // BY_REFERENSE
 
 
 void main()
 {
-	int a = 10, b = 6;
+	int a = 2.5, b = 3.14;
 	cout << a << "\t" << b << endl;
+#ifdef BY_POINTER
 	//Exchange(&a, &b);
-	Xor(&a, &b);
+	//	Xor(&a, &b);
+	cout << a << "\t" << b << endl;
+#endif // BY_POINTER
+
+	Xor<int>(a, b);
 	cout << a << "\t" << b << endl;
 }
+#ifdef BY_POINTER
+
+
 void Exchange(int* a, int* b)
 {
 	*a = *a + *b;
 	*b = *a - *b;
 	*a = *a - *b;
 }
+#endif // BY_POINTER
 template<typename T>
-void Xor(T*a, T*b)
+void Xor(int&a, int&b)
 {
-	*a ^= *b;
-	*b ^= *a;
-	*a ^= *b;
+	a ^= b;
+	b ^= a;
+	a ^= b;
 }
