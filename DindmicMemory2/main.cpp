695f709912d70b648aaa2329cf7c133f911b3ed1
Student <Student@304-1>
2019-11-04 21:09:54 +0200

    +

--- DindmicMemory2/main.cpp
+++ DindmicMemory2/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 void Print(int arr[], const int n);
+int* push_back(int arr[], int &n, int value);
+int* push_front(int arr[], int &n, int value);
+int* insert(int arr[], int &n, int value, int index);
+int* pop_front(int arr[], int &n);
+int* pop_back(int arr[], int &n);
+int*erase(int arr[], int &n, int index);
 
 void main()
 {
@@ -16,24 +22,35 @@ void main()
 	}
 	cout << endl;
 
-	// добавление элемента в конец массива
-	int value;
-	cout << "¬ведите добавл€емое значение: "; cin >> value;
-	// 1)создаЄм буферный массив, размером на 1 элемент больше 
-	int*buffer = new int[n + 1]{};
-	// 2)копируем содержимое исходного массива в буферный 
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	// 3) ”дал€ем исходный масив из пам€ти
-	delete[]arr;
-	// 4) в указатель исходного массива записываем адрес нового массива
-	arr = buffer;
-	arr[n] = value;
-		n++;
+	int value, chouse;
+	/*cout << "1. ƒобавить значение в начале массива " << endl << "2. ƒобавить значение в конце массива " << "3. ƒобавить значение в середину массива" << endl;
+	cin >> chouse;*/
+	//cout << "¬ведите добавл€емое значение: "; cin >> value;
+	//if (chouse == 1)
+	//{
+	//	arr = push_front(arr, n, value);
+	//}
+	//else if (chouse == 2)
+	//{
+	//	arr = push_back(arr, n, value);
+	//}
+	//else if (chouse == 3)
+	//{
+		int index;
+	cout << "¬ведите количество отступа: "; cin >> index;
+	//	cout << endl;
+	//	arr = insert(arr, n, value, index);
+	//}
+	//else
+	//{
+	//	cout << "Ќедопустимый выбор" << endl;
+	//}
+	arr = pop_front(arr, n);
 	Print(arr, n);
-
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	arr = erase(arr, n, index);
+		Print(arr, n);
 
 	delete[] arr;
 }
@@ -46,3 +63,89 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+
+int* push_back(int arr[], int &n, int value)
+{
+	int*buffer = new int[n + 1]{};
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[]arr;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
+}
+
+int* push_front(int arr[], int &n, int value)
+{
+	int*buffer = new int[n + 1];
+	for (int i = 0; i < n + 1; i++)
+	{
+		buffer[i] = arr[i - 1];
+	}
+	delete[]arr;
+	arr = buffer;
+	arr[0] = value;
+	n++;
+	return arr;
+}
+
+int* insert(int arr[], int &n, int value, int index)
+{
+	int*buffer = new int[n + 1];
+	for (int i = 0; i < index; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	for (int i = index; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	arr = buffer;
+	arr[index] = value;
+	n++;
+	return arr;
+}
+
+int* pop_front(int arr[], int &n)
+{
+	int*buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+
+int* pop_back(int arr[], int &n)
+{
+	int*buffer = new int[--n];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[]arr;
+	return buffer;
+}
+
+int*erase(int arr[], int &n, int index)
+{
+	int*buffer = new int[n - 1];
+	for (int i = 0; i < index; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	for (int i = index; i < n; i++)
+	{
+		buffer[i] = arr[i+1];
+	}
+	arr = buffer;
+	n--;
+	return arr;
+}
+
